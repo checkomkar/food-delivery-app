9ca692d7cda5f5b5468be6998535b1dd8cbcb6c9
Arjun K <check.omkar@gmail.com>
2022-06-11 15:28:53 +0530

    cookie options

--- pages/api/login.js
+++ pages/api/login.js
@@ -6,7 +6,8 @@ const sessionOptions = {
 	password: process.env.COOKIE_PASSWORD,
 	// secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
 	cookieOptions: {
-		secure: process.env.NODE_ENV === "production",
+		secure: true,
+		httpOnly: false,
 	},
 };
 
